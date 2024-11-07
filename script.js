eaae2ed32f8c7664f599af5e8c6757efb4613933
William Yao <143197278+anyadontfly@users.noreply.github.com>
2024-11-07 15:47:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-let phrases = [];
+let phrases = ["lab", "intern", "business"];
+let phraseCounter = 0;
 
-const inputPage = document.getElementById('input-page');
-const raindropPage = document.getElementById('raindrop-page');
-const wordInput = document.getElementById('word-input');
+const startPage = document.getElementById('start-page');
+const internPage = document.getElementById('intern-page');
+const businessPage = document.getElementById('business-page');
+const brokePage = document.getElementById('broke-page');
+
 const startButton = document.getElementById('start-button');
+const internButton = document.getElementById('intern-button');
+const businessButton = document.getElementById('business-button');
+const brokeButton = document.getElementById('broke-button');
 
-let raindrops = [];
+const raindropPageLab = document.getElementById('raindrop-page-lab');
+const raindropPageIntern = document.getElementById('raindrop-page-intern');
+const raindropPageBusiness = document.getElementById('raindrop-page-business');
+
+let raindropsLab = [];
+let raindropsEliminatedLab = 0;
+
+let raindropsIntern = [];
+let raindropsEliminatedIntern = 0;
+
+let raindropsBusiness = [];
+let raindropsEliminatedBusiness = 0;
 
 let cursorX = 0;
 let cursorY = 0;
@@ -15,36 +32,70 @@ document.addEventListener('mousemove', (event) => {
 });
 
 startButton.addEventListener('click', () => {
-    const inputText = wordInput.value.trim();
-    if (inputText) {
-        phrases = inputText.split(',').map(word => word.trim());
-        inputPage.style.display = 'none';
-        raindropPage.style.display = 'block';
-        startRaindropAnimation();
-    } else {
-        alert('Please enter at least one word.');
-    }
+    startPage.style.display = 'none';
+    internPage.style.display = 'none';
+    businessPage.style.display = 'none';
+    brokePage.style.display = 'none';
+    raindropPageLab.style.display = 'block';
+    raindropPageIntern.style.display = 'none';
+    raindropPageBusiness.style.display = 'none';
+    removeAllRaindrops()
+    startRaindropAnimationLab();
 });
 
-function startRaindropAnimation() {
+internButton.addEventListener('click', () => {
+    startPage.style.display = 'none';
+    internPage.style.display = 'none';
+    businessPage.style.display = 'none';
+    brokePage.style.display = 'none';
+    raindropPageLab.style.display = 'none';
+    raindropPageIntern.style.display = 'block';
+    raindropPageBusiness.style.display = 'none';
+    removeAllRaindrops()
+    startRaindropAnimationIntern();
+});
+
+businessButton.addEventListener('click', () => {
+    startPage.style.display = 'none';
+    internPage.style.display = 'none';
+    businessPage.style.display = 'none';
+    brokePage.style.display = 'none';
+    raindropPageLab.style.display = 'none';
+    raindropPageIntern.style.display = 'none';
+    raindropPageBusiness.style.display = 'block';
+    removeAllRaindrops()
+    startRaindropAnimationBusiness();
+});
+
+brokeButton.addEventListener('click', () => {
+    // startPage.style.display = 'block';
+    // internPage.style.display = 'none';
+    // businessPage.style.display = 'none';
+    // brokePage.style.display = 'none';
+    // raindropPageLab.style.display = 'none';
+    // raindropPageIntern.style.display = 'none';
+    // raindropPageBusiness.style.display = 'none';
+    // removeAllRaindrops()
+    location.reload();
+});
+
+function startRaindropAnimationLab() {
+    console.log("in");
     function createRaindrop() {
         const raindrop = document.createElement('div');
         raindrop.classList.add('raindrop');
 
-        raindrop.textContent = phrases[Math.floor(Math.random() * phrases.length)];
-
+        raindrop.textContent = phrases[0];
         raindrop.style.left = Math.random() * 100 + 'vw';
-
-        raindrop.style.animationDuration = ((Math.random() * 10 + 4))/4 + 's';
-
+        raindrop.style.animationDuration = ((Math.random() * 10 + 4)) / 4 + 's';
         raindrop.style.fontSize = (Math.random() * 30 + 10) + 'px';
 
-        raindropPage.appendChild(raindrop);
-        raindrops.push(raindrop);
+        raindropPageLab.appendChild(raindrop);
+        raindropsLab.push(raindrop);
 
-        if (raindrops.length > 300) {
+        if (raindropsLab.length > 150) {
             for (let i = 0; i < 5; i++) {
-                const oldRaindrop = raindrops.shift();
+                const oldRaindrop = raindropsLab.shift();
                 if (oldRaindrop && oldRaindrop.parentNode) {
                     oldRaindrop.remove();
                 }
@@ -58,6 +109,10 @@ function startRaindropAnimation() {
                 raindrop.style.color = 'red';
                 raindrop.classList.add('near-cursor');
             }
+            if (distance >= 100) {
+                raindrop.style.color = 'blue';
+                raindrop.classList.remove('near-cursor');
+            }
         }
 
         const proximityInterval = setInterval(() => {
@@ -66,7 +121,105 @@ function startRaindropAnimation() {
 
         raindrop.addEventListener('animationend', () => {
             clearInterval(proximityInterval);
-            raindrops = raindrops.filter(r => r !== raindrop);
+            raindropsLab = raindropsLab.filter(r => r !== raindrop);
+            raindrop.remove();
+        });
+    }
+
+    setInterval(createRaindrop, 20);
+}
+
+function startRaindropAnimationIntern() {
+    function createRaindrop() {
+        const raindrop = document.createElement('div');
+        raindrop.classList.add('raindrop');
+
+        raindrop.textContent = phrases[1];
+        raindrop.style.left = Math.random() * 100 + 'vw';
+        raindrop.style.animationDuration = ((Math.random() * 10 + 4)) / 4 + 's';
+        raindrop.style.fontSize = (Math.random() * 30 + 10) + 'px';
+
+        raindropPageIntern.appendChild(raindrop);
+        raindropsIntern.push(raindrop);
+
+        if (raindropsIntern.length > 150) {
+            for (let i = 0; i < 5; i++) {
+                const oldRaindrop = raindropsIntern.shift();
+                if (oldRaindrop && oldRaindrop.parentNode) {
+                    oldRaindrop.remove();
+                }
+            }
+        }
+
+        function checkProximity() {
+            const rect = raindrop.getBoundingClientRect();
+            const distance = Math.hypot(cursorX - (rect.left + rect.width / 2), cursorY - (rect.top + rect.height / 2));
+            if (distance < 100) {
+                raindrop.style.color = 'red';
+                raindrop.classList.add('near-cursor');
+            }
+            if (distance >= 100) {
+                raindrop.style.color = 'blue';
+                raindrop.classList.remove('near-cursor');
+            }
+        }
+
+        const proximityInterval = setInterval(() => {
+            checkProximity();
+        }, 100);
+
+        raindrop.addEventListener('animationend', () => {
+            clearInterval(proximityInterval);
+            raindropsIntern = raindropsIntern.filter(r => r !== raindrop);
+            raindrop.remove();
+        });
+    }
+
+    setInterval(createRaindrop, 20);
+}
+
+function startRaindropAnimationBusiness() {
+    function createRaindrop() {
+        const raindrop = document.createElement('div');
+        raindrop.classList.add('raindrop');
+
+        raindrop.textContent = phrases[2];
+        raindrop.style.left = Math.random() * 100 + 'vw';
+        raindrop.style.animationDuration = ((Math.random() * 10 + 4)) / 4 + 's';
+        raindrop.style.fontSize = (Math.random() * 30 + 10) + 'px';
+
+        raindropPageBusiness.appendChild(raindrop);
+        raindropsBusiness.push(raindrop);
+
+        if (raindropsBusiness.length > 150) {
+            for (let i = 0; i < 5; i++) {
+                const oldRaindrop = raindropsBusiness.shift();
+                if (oldRaindrop && oldRaindrop.parentNode) {
+                    oldRaindrop.remove();
+                }
+            }
+        }
+
+        function checkProximity() {
+            const rect = raindrop.getBoundingClientRect();
+            const distance = Math.hypot(cursorX - (rect.left + rect.width / 2), cursorY - (rect.top + rect.height / 2));
+            if (distance < 100) {
+                raindrop.style.color = 'red';
+                raindrop.classList.add('near-cursor');
+            }
+            if (distance >= 100) {
+                raindrop.style.color = 'blue';
+                raindrop.classList.remove('near-cursor');
+            }
+        }
+
+        const proximityInterval = setInterval(() => {
+            checkProximity();
+        }, 100);
+
+        raindrop.addEventListener('animationend', () => {
+            clearInterval(proximityInterval);
+            raindropsBusiness = raindropsBusiness.filter(r => r !== raindrop);
             raindrop.remove();
         });
     }
@@ -75,11 +228,95 @@ function startRaindropAnimation() {
 }
 
 document.addEventListener('click', () => {
-    raindrops = raindrops.filter(raindrop => {
-        if (raindrop.classList.contains('near-cursor')) {
-            raindrop.remove();
-            return false;
-        }
-        return true;
-    });
+    if (phraseCounter == 0) {
+        raindropsLab = raindropsLab.filter(raindrop => {
+            if (raindrop.classList.contains('near-cursor')) {
+                raindrop.remove();
+                raindropsEliminatedLab++;
+                if (raindropsEliminatedLab >= 20) {
+                    raindropsEliminatedLab = 0;
+                    phraseCounter++;
+                    removeAllRaindrops()
+                    startPage.style.display = 'none';
+                    internPage.style.display = 'block';
+                    businessPage.style.display = 'none';
+                    brokePage.style.display = 'none';
+                    raindropPageLab.style.display = 'none';
+                    raindropPageIntern.style.display = 'none';
+                    raindropPageBusiness.style.display = 'none';
+                }
+                return false;
+            }
+            return true;
+        });
+    }
+
+    if (phraseCounter == 1) {
+        raindropsIntern = raindropsIntern.filter(raindrop => {
+            if (raindrop.classList.contains('near-cursor')) {
+                raindrop.remove();
+                raindropsEliminatedIntern++;
+                if (raindropsEliminatedIntern >= 20) {
+                    raindropsEliminatedIntern = 0;
+                    phraseCounter++;
+                    removeAllRaindrops()
+                    startPage.style.display = 'none';
+                    internPage.style.display = 'none';
+                    businessPage.style.display = 'block';
+                    brokePage.style.display = 'none';
+                    raindropPageLab.style.display = 'none';
+                    raindropPageIntern.style.display = 'none';
+                    raindropPageBusiness.style.display = 'none';
+                }
+                return false;
+            }
+            return true;
+        });
+    }
+
+    if (phraseCounter == 2) {
+        raindropsBusiness = raindropsBusiness.filter(raindrop => {
+            if (raindrop.classList.contains('near-cursor')) {
+                raindrop.remove();
+                raindropsEliminatedBusiness++;
+                if (raindropsEliminatedBusiness >= 20) {
+                    raindropsEliminatedBusiness = 0;
+                    phraseCounter++;
+                    removeAllRaindrops()
+                    startPage.style.display = 'none';
+                    internPage.style.display = 'none';
+                    businessPage.style.display = 'none';
+                    brokePage.style.display = 'block';
+                    raindropPageLab.style.display = 'none';
+                    raindropPageIntern.style.display = 'none';
+                    raindropPageBusiness.style.display = 'none';
+                }
+                return false;
+            }
+            return true;
+        });
+    }
 });
+
+function removeAllRaindrops() {
+    raindropsLab.forEach(raindrop => {
+        if (raindrop && raindrop.parentNode) {
+            raindrop.remove();
+        }
+    });
+    raindropsLab = [];
+
+    raindropsIntern.forEach(raindrop => {
+        if (raindrop && raindrop.parentNode) {
+            raindrop.remove();
+        }
+    });
+    raindropsIntern = [];
+
+    raindropsBusiness.forEach(raindrop => {
+        if (raindrop && raindrop.parentNode) {
+            raindrop.remove();
+        }
+    });
+    raindropsBusiness = [];
+}
